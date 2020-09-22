e21b7d719603757f2c56f20de4bde5fd7d60c65d
kellivalverde <paughka@gmail.com>
2020-09-22 16:20:40 -0400

    completed 08

--- 08 - Fun with HTML5 Canvas/js/scripts.js
+++ 08 - Fun with HTML5 Canvas/js/scripts.js
@@ -8,18 +8,20 @@ ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
+ctx.globalCompositeOperation = 'multiply'; // like Photoshop blend modes 
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let direction = true; // building up - ascending
 
 
-function draw(e){  //e = event
-   if (!isDrawing) return; // stops function from running when mouse is not down
+function draw(e) { //e = event
+    if (!isDrawing) return; // stops function from running when mouse is not down
     console.log(e);
     ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-
+    // ctx.lineWidth = hue;  -- handled below instead
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX, lastY);
@@ -27,17 +29,29 @@ function draw(e){  //e = event
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY]; // new ES6 way to write this
-    hue++;
+
+    hue++; // adds 1 to color - changes hue
+    if (hue >= 360) {
+        hue = 0; // resets hue
+    }
+
+    //for every loop, we will increment the line width, but add conditions so it doesn't keep growing infinately
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction; //flip the direction
+    }
+    if (direction) {
+        ctx.lineWidth++; //increment
+    } else {
+        ctx.lineWidth--; //decrement
+    }
 }
 
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
-  });
-  
-  
-  canvas.addEventListener('mousemove', draw);
-  canvas.addEventListener('mouseup', () => isDrawing = false);
-  canvas.addEventListener('mouseout', () => isDrawing = false);
-  
+});
 
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
