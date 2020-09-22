c042b08774572c343644abb24f53ae103df7cf9d
kellivalverde <paughka@gmail.com>
2020-09-22 15:18:12 -0400

    making progress

--- 08 - Fun with HTML5 Canvas/js/scripts.js
+++ 08 - Fun with HTML5 Canvas/js/scripts.js
@@ -15,7 +15,25 @@ let lastY = 0;
 
 
 function draw(e){  //e = event
+   if (!isDrawing) return; // stops function from running when mouse is not down
     console.log(e);
+
+    ctx.beginPath();
+
+    //start from
+    ctx.moveTo(lastX, lastY);
+    //got to 
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+
 }
 
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+
+
+
