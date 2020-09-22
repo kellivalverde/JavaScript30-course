164061a563e64be0ce577bf9ed65febc28d36d4a
kellivalverde <paughka@gmail.com>
2020-09-22 15:48:03 -0400

    got stroke to work correctly

--- 08 - Fun with HTML5 Canvas/js/scripts.js
+++ 08 - Fun with HTML5 Canvas/js/scripts.js
@@ -1,13 +1,13 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d'); // ctx = the context for the canvas
 
-//  vvv handled in css vvv
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
@@ -19,21 +19,23 @@ function draw(e){  //e = event
     console.log(e);
 
     ctx.beginPath();
-
-    //start from
+    // start from
     ctx.moveTo(lastX, lastY);
-    //got to 
+    // go to
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
-    lastX = e.offsetX;
-    lastY = e.offsetY;
+    [lastX, lastY] = [e.offsetX, e.offsetY]; // new ES6 way to write this
 
 }
 
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-
-
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  });
+  
+  
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mouseup', () => isDrawing = false);
+  canvas.addEventListener('mouseout', () => isDrawing = false);
+  
 
